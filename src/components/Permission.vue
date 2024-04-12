e2dd4b3ee7ff84a2959b472b3e72085422a14fbe
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-12 22:51:18 +0630

    permission module checkbox

--- src/components/Permission.vue
+++ src/components/Permission.vue
@@ -2,9 +2,20 @@
   <el-tabs type="border-card">
     <el-tab-pane label="User">
       <el-table :data="tableData" stripe style="width: 100%">
-        <el-table-column prop="date" label="Date" width="180" />
-        <el-table-column prop="name" label="Name" width="180" />
-        <el-table-column prop="address" label="Address" />
+        <el-table-column prop="module" label="Module" width="180" />
+        <el-table-column label="All" width="180">
+          <template #default>
+            <el-checkbox
+              v-model="checkAll"
+              :indeterminate="isIndeterminate"
+              @change="handleCheckAllChange"
+            ></el-checkbox>
+          </template>
+        </el-table-column>
+        <el-table-column prop="create" label="Create" />
+        <el-table-column prop="read" label="Read" />
+        <el-table-column prop="update" label="Update" />
+        <el-table-column prop="delete" label="Delete" />
       </el-table>
     </el-tab-pane>
     <el-tab-pane label="Config">Config</el-tab-pane>
@@ -13,26 +24,70 @@
   </el-tabs>
 </template>
 <script lang="ts" setup>
+import { ref } from "vue";
+const checkAll = ref(false);
+const isIndeterminate = ref(true);
+const checkedCities = ref(["Shanghai", "Beijing"]);
+const cities = ["Shanghai", "Beijing", "Guangzhou", "Shenzhen"];
+
+const handleCheckAllChange = (val: boolean) => {
+  checkedCities.value = val ? cities : [];
+  isIndeterminate.value = false;
+};
+const handleCheckedCitiesChange = (value: string[]) => {
+  const checkedCount = value.length;
+  checkAll.value = checkedCount === cities.length;
+  isIndeterminate.value = checkedCount > 0 && checkedCount < cities.length;
+};
+
 const tableData = [
   {
-    date: "2016-05-03",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
+    module: "Product",
+    all: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
   },
   {
-    date: "2016-05-02",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
+    module: "User",
+    all: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
   },
   {
-    date: "2016-05-04",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
+    module: "Order",
+    all: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
   },
   {
-    date: "2016-05-01",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
+    module: "Role",
+    all: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
+  },
+  {
+    module: "Permission",
+    all: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
+  },
+  {
+    module: "UserRolePermission",
+    all: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
   },
 ];
 </script>
