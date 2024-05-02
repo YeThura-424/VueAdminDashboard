cd0c7f7fbd6fcd59c65e70e08074806f74e4561c
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-02 21:47:14 +0630

    permission module split into tabs

--- src/components/Permission.vue
+++ src/components/Permission.vue
@@ -1,6 +1,6 @@
 <template>
   <el-tabs type="border-card">
-    <el-tab-pane label="User">
+    <el-tab-pane label="General">
       <div class="permission-module">
         <table id="permission-table">
           <thead>
@@ -12,7 +12,10 @@
             <th>Delete</th>
           </thead>
           <tbody>
-            <tr v-for="(permission, index) in permissions" :key="index">
+            <tr
+              v-for="(permission, index) in permissions[0].general"
+              :key="index"
+            >
               <td>{{ permission.module }}</td>
               <td>
                 <el-checkbox
@@ -45,9 +48,54 @@
         </table>
       </div>
     </el-tab-pane>
-    <el-tab-pane label="Config">Config</el-tab-pane>
-    <el-tab-pane label="Role">Role</el-tab-pane>
-    <el-tab-pane label="Task">Task</el-tab-pane>
+    <el-tab-pane label="Authentication">
+      <div class="permission-module">
+        <table id="permission-table">
+          <thead>
+            <th>Module</th>
+            <th>Check All</th>
+            <th>Create</th>
+            <th>Read</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(permission, index) in permissions[0].authentication"
+              :key="index"
+            >
+              <td>{{ permission.module }}</td>
+              <td>
+                <el-checkbox
+                  v-model="permissionVAl.all[permission.model_value]"
+                  @change="checkAllPermissions(permission.model_value)"
+                />
+              </td>
+              <td>
+                <el-checkbox
+                  v-model="permissionVAl.create[permission.model_value]"
+                />
+              </td>
+              <td>
+                <el-checkbox
+                  v-model="permissionVAl.read[permission.model_value]"
+                />
+              </td>
+              <td>
+                <el-checkbox
+                  v-model="permissionVAl.update[permission.model_value]"
+                />
+              </td>
+              <td>
+                <el-checkbox
+                  v-model="permissionVAl.delete[permission.model_value]"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script lang="ts" setup>
@@ -55,7 +103,7 @@ import { reactive, watch } from "vue";
 import permissions from "../dummy/permissionModule.js";
 
 const emit = defineEmits(["submitPermissions"]);
-
+console.log(permissions[0].general);
 const permissionVAl = reactive({
   all: [],
   create: [],
