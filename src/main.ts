2c2e1d89be42127ed565e84934a5cce4d9e74b03
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-05 13:12:26 +0630

    dark theme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import "./style/element/index.scss";
 
 import "./style.css";
+import "./style/index.scss";
+
 import App from "./App.vue";
 import router from "./router";
 const pinia = createPinia();
