6835ca36b4fc8320d79faf60f296c5f12b2d86bc
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-29 23:02:50 +0630

    dashboard layout

---
+++ src/pages/Dashboard1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-3 gap-7">
+    <div class="col-span-1">
+      <div class="best-seller">
+        <div class="static text-[#2f2b3db3]">
+          <h1 class="font-medium text-lg py-2">Congratulation John!</h1>
+          <p class="text-xs pb-4">Best seller of the month</p>
+          <h1 class="text-[#7367f0] font-2xl">$48.9K</h1>
+          <el-button type="primary">View Sales</el-button>
+        </div>
+        <div class="avarter">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+    <div class="col-span-2"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
