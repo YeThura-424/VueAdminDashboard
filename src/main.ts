e5ad829287de047e4771198b4ab4123152c989c1
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-25 19:53:54 +0630

    add auto resolver for path

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { createPinia } from "pinia";
 import VueApexCharts from "vue3-apexcharts";
 
 import "./style.css";
-import "./style/index.scss";
+// import "./style/index.scss";
 
 import App from "./App.vue";
-import router from "./router";
+import router from "@/router";
 const pinia = createPinia();
 
 createApp(App).use(router).use(pinia).use(VueApexCharts).mount("#app");
